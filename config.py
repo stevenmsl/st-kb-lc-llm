e04b12774d30a15c325ac85aae4cb30896b70d19
Steven Lin <Steven.Lin@appliedsystems.com>
2024-02-19 09:44:32 -0800

    Refactored environment variable management: Replaced loadenv package with TOML file

--- config.py
+++ config.py
@@ -1,7 +1,37 @@
-from dotenv import load_dotenv
-import os
+import toml  
+import os  
+import streamlit as st  
 
-load_dotenv()
+# The package loadenv was previously used to manage environment variables. However, Streamlit only accepts TOML files for   
+# managing secrets in a secure manner. Therefore, the loadenv package was dropped in favor of using a TOML file.   
+# The TOML file is either read directly if the "st.secrets" object is not populated (i.e., in a local environment),   
+# or the environment variables are populated from the "st.secrets" object (i.e., in a Streamlit sharing environment).   
+# This way, we ensure compatibility with Streamlit's secrets management and allow for secure handling of sensitive data   
+# in different environments.
+
+# The secrets.toml file must be positioned within the .streamlit directory.
+
+if st.secrets:  
+    os.environ["OPENAI_API_KEY"] = st.secrets["openai"]["api_key"]  
+    os.environ["OPENAI_API_TYPE"] = st.secrets["openai"]["api_type"]  
+    os.environ["OPENAI_API_VERSION"] = st.secrets["openai"]["api_version"]  
+    os.environ["AZURE_OPENAI_DEFAULT_DEPLOYMENT_NAME"] = st.secrets["azure_openai"]["default_deployment_name"]  
+    os.environ["AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME"] = st.secrets["azure_openai"]["embedding_deployment_name"]  
+    os.environ["AZURE_OPENAI_ENDPOINT"] = st.secrets["azure_openai"]["endpoint"]  
+    os.environ["AZURE_OPENAI_GPT35TURBO_DEPLOYMENT_NAME"] = st.secrets["azure_openai"]["gpt35turbo_deployment_name"]  
+    os.environ["AZURE_OPENAI_GPT4_DEPLOYMENT_NAME"] = st.secrets["azure_openai"]["gpt4_deployment_name"]  
+    os.environ["PYTHONIOENCODING"] = st.secrets["other"]["pythonioencoding"]  
+else:  
+    secrets = toml.load("./secrets.toml")  
+    os.environ["OPENAI_API_KEY"] = secrets["openai"]["api_key"]  
+    os.environ["OPENAI_API_TYPE"] = secrets["openai"]["api_type"]  
+    os.environ["OPENAI_API_VERSION"] = secrets["openai"]["api_version"]  
+    os.environ["AZURE_OPENAI_DEFAULT_DEPLOYMENT_NAME"] = secrets["azure_openai"]["default_deployment_name"]  
+    os.environ["AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME"] = secrets["azure_openai"]["embedding_deployment_name"]  
+    os.environ["AZURE_OPENAI_ENDPOINT"] = secrets["azure_openai"]["endpoint"]  
+    os.environ["AZURE_OPENAI_GPT35TURBO_DEPLOYMENT_NAME"] = secrets["azure_openai"]["gpt35turbo_deployment_name"]  
+    os.environ["AZURE_OPENAI_GPT4_DEPLOYMENT_NAME"] = secrets["azure_openai"]["gpt4_deployment_name"]  
+    os.environ["PYTHONIOENCODING"] = secrets["other"]["pythonioencoding"] 
 
 AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 AZURE_OPENAI_GPT4_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_GPT4_DEPLOYMENT_NAME")
@@ -10,4 +40,4 @@ AZURE_OPENAI_DEFAULT_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEFAULT_DEPLOYMEN
 AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME")
 OPENAI_API_TYPE = os.getenv("OPENAI_API_TYPE")
 OPENAI_API_VERSION = os.getenv("OPENAI_API_VERSION")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY") 
